2b2e13ffeec559e22ac7b411ce2df7f5e9a786f1
Pablo Pérez Martín <aperezvic21@gmail.com>
2023-01-29 20:49:39 +0100

    Working sensor transmision through Esp8266

--- IMUReadings/IMUReadingsBuffered/IMUReadingsBuffered.ino
+++ IMUReadings/IMUReadingsBuffered/IMUReadingsBuffered.ino
@@ -2,31 +2,34 @@
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
 #include <SoftwareSerial.h>
-#include <arduino-timer.h>
 
 #define STACK_SIZE 20
 
 const int buttonPin = 2;
 const int ledPin = 3;
-String stack[STACK_SIZE];
 
-int top = -1;
+//Counter for the amount of sequences sent
+int counter = 0;
 int buttonState = 0;
-//Timer to control recording time of movement
-auto timer = timer_create_default();
+//Led to notify gesture recording
+bool ledOn = false;
+//Control wether pushing the button does anything at all
+bool canPushButton = true;
 
 Adafruit_MPU6050 mpu;
 
-SoftwareSerial mySerial(10, 11); // RX, TX
+SoftwareSerial mySerial(10, 11);  // RX, TX
 
 void setup(void) {
+
+  Serial.println("Starting program...");
 
   pinMode(buttonPin, INPUT);
   pinMode(ledPin, OUTPUT);
 
   Serial.begin(115200);
   while (!Serial)
-    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+    delay(10);  // will pause Zero, Leonardo, etc until serial console opens
 
 
   // Try to initialize!
@@ -48,107 +51,54 @@ void setup(void) {
 
   //Serial Comunication with esp8266
   mySerial.begin(115200);
-  mySerial.println("Comenzando conexion");
+  mySerial.println("Starting conexion");
 }
 
-bool send = true;
 
 void loop() {
 
-  //Timer tick
-  timer.tick();
 
   /* Get new sensor events with the readings */
-  sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
+  //if recording
+  if (ledOn) {
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
 
-  String values = "";
-  values = String(a.acceleration.x) + "," + String(a.acceleration.y) + "," + String(a.acceleration.z) + "," + String(g.gyro.x) + "," + String(g.gyro.y) +"," + String(g.gyro.z) ;
-  
-  //detectar si hacemos un movimiento que pueda ser un gesto (quizas luego se implemente con un boton)
-  if(a.acceleration.x > 15 || a.acceleration.y > 15 || a.acceleration.z > 15){
-    if(send){
-      //Serial.println("Se ha iniciado un movimiento");
-      send = false;
-      //dejamos solo 4 secuencias en el buffer
-      while(top > 4){
-        pop();
-      }
+    String values = "";
+    values = String(a.acceleration.x) + "," + String(a.acceleration.y) + "," + String(a.acceleration.z) + "," + String(g.gyro.x) + "," + String(g.gyro.y) + "," + String(g.gyro.z);
+
+    //Send over serial
+    mySerial.println(values);
+    counter++;
+
+    Serial.println(counter);
+    Serial.println(values);
+    if (counter == STACK_SIZE) {
+      //if buffer fills stop
+      toggle_led();
+      //restart counter
+      counter = 0;
+      //printStackToSerial();
     }
-
   }
 
 
-  if(top == STACK_SIZE - 1){
-    //que sigan pudiendo entrar valores 
-    pop();
-  }
-  //colocamos nuevo valor al principio
-  push_at_beginning(values);
-
-  if(!send && top == STACK_SIZE - 1){
-
-    Serial.println("Logeando movimiento");
-    Serial.println("Valor antes top: " + top);
-    //TODO: tenemos que enviar lo que tengamos en el buffer
-    //de momento lo printeamos
-    for(int i = 0;i<20;i++){
-      String val = pop();
-      mySerial.println(val);
-      Serial.println(val);
-    }
-
-    send = true;
-    Serial.println("Valor de top: " + top);
-    delay(1000);
-  }
-
-  /* //mandamos al serial del esp8266 01
-  mySerial.println(values);
-  //printeamos por nuestro serial
-  Serial.println(values); */
-
-  //boton
+  //Button
   buttonState = digitalRead(buttonPin);
 
-  if (buttonState == HIGH) {
-    Serial.println("Boton pulsado");
-  } 
+  if (buttonState == HIGH && canPushButton) {
+    toggle_led();
+    mySerial.println("Sequence");
+  }
 
 
+  //Pick measurements each 100 ms
   delay(100);
 }
 
-
 //Toggle LED
-bool toggle_led(void *) {
-  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN)); // toggle the LED
-  return true; // keep timer active? true
-}
-
-//metodos para el stack
-void push(String item) {
-  if (top == STACK_SIZE - 1) {
-    Serial.println("Error: Stack overflow");
-    return;
-  }
-  stack[++top] = item;
-}
-
-void push_at_beginning(String item) {
-  if (top == STACK_SIZE - 1) {
-    Serial.println("Error: Stack overflow");
-    return;
-  }
-  memmove(stack+1, stack, sizeof(String)*(top+1));
-  stack[0] = item;
-  top++;
-}
-
-String pop() {
-  if (top == -1) {
-    Serial.println("Error: Stack underflow");
-    return "";
-  }
-  return stack[top--];
+void toggle_led() {
+  ledOn = canPushButton;
+  canPushButton = !canPushButton;
+  digitalWrite(ledPin, ledOn);  // toggle the LED
 }
